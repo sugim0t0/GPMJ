f550a49ce8ace17f3e9bd79cd9b156b8145f4327
sugim0t0 <sugimoto.shun@gmail.com>
2017-04-15 23:30:26 +0900

    Add gpmj/gpmj.py

---
+++ gpmj/gpmj.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+
+''' GPMJ (General Purpose (Japanese) Mah-Jong library)
+
+Modification History:
+===========================================================
+Date           Version   Description
+===========================================================
+28 Mar. 2017   0.1       Creation
+-----------------------------------------------------------
+'''
+
+from enum import Enum, IntEnum
+
+__version__ = "0.1"
+__date__ = "28 Mar. 2017"
+__author__ = "Shun SUGIMOTO <sugimoto.shun@gmail.com>"
+
+class Suits(IntEnum):
+
+    # Simples
+    DOTS           = 0
+    BAMBOO         = 1
+    CHARACTERS     = 2
+    # Honors
+    WINDS          = 3
+    DRAGONS        = 4
+
+    NUM_OF_SUITS   = 5
+    NUM_OF_SIMPLES = 3
+
+    def __str__(self):
+        if self.value == Suits.DOTS.value:
+            return "D"
+        elif self.value == Suits.BAMBOO.value:
+            return "B"
+        elif self.value == Suits.CHARACTERS.value:
+            return "C"
+
+
+class Winds(IntEnum):
+
+    EAST           = 0
+    SOUTH          = 1
+    WEST           = 2
+    NORTH          = 3
+    NUM_OF_WINDS   = 4
+
+    def __str__(self):
+        if self.value == Winds.EAST.value:
+            return "Es"
+        elif self.value == Winds.SOUTH.value:
+            return "St"
+        elif self.value == Winds.WEST.value:
+            return "Ws"
+        elif self.value == Winds.NORTH.value:
+            return "Nt"
+
+
+class Dragons(IntEnum):
+
+    WHITE          = 0
+    GREEN          = 1
+    RED            = 2
+    NUM_OF_DRAGONS = 3
+
+    def __str__(self):
+        if self.value == Dragons.WHITE.value:
+            return "Wh"
+        elif self.value == Dragons.GREEN.value:
+            return "Gr"
+        elif self.value == Dragons.RED.value:
+            return "Rd"
+
+
+class Tile():
+
+    def __init__(self, suit, number, b_red):
+        self.suit = suit
+        self.number = number
+        self.b_red = b_red
+        # Print characters
+        self.print_char = '['
+        if suit == Suits.WINDS:
+            self.print_char += str(Winds(number))
+        elif suit == Suits.DRAGONS:
+            self.print_char += str(Dragons(number))
+        else:
+            self.print_char += str(Suits(suit))
+            self.print_char += str(self.number)
+        self.print_char += ']'
+
+
+class Meld():
+
+    def __init__(self):
+        self.tiles = []
+        self.b_sequential = False
+
+    def reset(self):
+        self.tiles = []
+        self.b_sequential = False
+
+    def add_tile(self, tile):
+        if len(self.tiles) == 0:
+            self.tiles.append(tile)
+        elif not self.tiles[0].suit == tile.suit:
+            return False
+        elif len(self.tiles) == 1:
+            if self.tiles[0].number == tile.number:
+                self.tiles.append(tile)
+            elif tile.suit < Suits.WINDS and \
+                 abs(self.tiles[0].number - tile.number) <= 2:
+                self.b_sequential = True
+                if self.tiles[0].number > tile.number:
+                    self.tiles.insert(0, tile)
+                else:
+                    self.tiles.append(tile)
+            else:
+                return False
+        elif len(self.tiles) == 2:
+            if self.b_sequential:
+                if self.tiles[1].number - self.tiles[0].number == 1:
+                    if (self.tiles[0].number - 1) == tile.number:
+                        self.tiles.insert(0, tile)
+                    elif (self.tiles[1].number + 1) == tile.number:
+                        self.tiles.append(tile)
+                    else:
+                        return False
+                else:
+                    if (self.tiles[0].number + 1) == tile.number:
+                        self.tiles.insert(1, tile)
+                    else:
+                        return False
+            elif self.tiles[0].number == tile.number:
+                self.tiles.append(tile)
+            else:
+                return False
+        elif len(self.tiles) == 3:
+            if self.b_sequential:
+                return False
+            if self.tiles[0].number == tile.number:
+                self.tiles.append(tile)
+            else:
+                return False
+        else:
+            return False
+        return True
+
+
+class Hand():
+
+    def __init__(self):
+        self.pure_tiles = [[], [], [], [], []]
+        self.exposed = []
+        self.required = [[], [], [], [], []]
+
+    def append_tile(self, tile):
+        self.pure_tiles[tile.suit].append(tile)
+
+    def pop_tile(self, suit, index):
+        return self.pure_tiles[suit].pop(index)
+
+    def sort_tiles(self):
+        # Bubble sort
+        for suit in range(Suits.NUM_OF_SUITS):
+            if len(self.pure_tiles[suit]) > 1:
+                for i in range(len(self.pure_tiles[suit])-1):
+                    for j in range(len(self.pure_tiles[suit])-1, i, -1):
+                        if self.pure_tiles[suit][j].number < self.pure_tiles[suit][j-1].number:
+                            self.pure_tiles[suit][j], self.pure_tiles[suit][j-1] = \
+                            self.pure_tiles[suit][j-1], self.pure_tiles[suit][j]
+
+    def print_tiles(self):
+        for suit in range(Suits.NUM_OF_SUITS):
+            for tile in self.pure_tiles[suit]:
+                print(tile.print_char, end="")
+        print("")
+
+    def get_required_13orphans(self):
+        required = [[], [], [], [], []]
+        b_missed = False
+        if not(len(self.pure_tiles) == 13):
+            return None
+        self.sort_tiles()
+        # Simples (Dots, Bamboo, Characters)
+        for suit in range(Suits.NUM_OF_SIMPLES):
+            required[suit].append(1)
+            required[suit].append(9)
+            for tile in self.pure_tiles[suit]:
+                if tile.number > 1 and tile.number < 9:
+                    return None
+                required[suit].remove(tile.number)
+            if len(required[suit]) > 0:
+                if b_missed or len(required[suit]) > 1:
+                    return None
+                b_missed = True
+        # Winds
+        for wind in range(Winds.NUM_OF_WINDS):
+            required[Suits.WINDS].append(wind)
+        for tile in self.pure_tiles[Suits.WINDS]:
+            required[Suits.WINDS].remove(tile.number)
+        if len(required[Suits.WINDS]) > 0:
+            if b_missed or len(required[Suits.WINDS]) > 1:
+                return None
+            b_missed = True
+        # Dragons
+        for dragon in range(Dragons.NUM_OF_DRAGONS):
+            required[Suits.DRAGONS].append(dragon)
+        for tile in self.pure_tiles[Suits.DRAGONS]:
+            required[Suits.DRAGONS].remove(tile.number)
+        if len(required[Suits.DRAGONS]) > 0:
+            if b_missed or len(required[Suits.DRAGONS]) > 1:
+                return None
+            b_missed = True
+        if not b_missed:
+            for suit in range(Suits.NUM_OF_SIMPLES):
+                required[suit].append(1)
+                required[suit].append(9)
+            for wind in range(Winds.NUM_OF_WINDS):
+                required[Suits.WINDS].append(wind)
+            for dragon in range(Dragons.NUM_OF_DRAGONS):
+                required[Suits.DRAGONS].append(dragon)
+        return required
+
+
