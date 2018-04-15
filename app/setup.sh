148cabc3730069fbb8c45e407a4066cde08505e3
sugim0t0 <sugimoto.shun@gmail.com>
2018-04-15 20:39:23 +0900

    move sample cui app to app/cui_app/

--- app/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ln -s ../gpmj/gpmjcore.py gpmjcore.py
-ln -s ../gpmj/gpmjgame.py gpmjgame.py
-ln -s ../gpmj/gpmjplayer.py gpmjplayer.py
-ln -s ../gpmj/gpmjctrl.py gpmjctrl.py
-ln -s ../gpmj/gpmj.cfg gpmj.cfg
-
